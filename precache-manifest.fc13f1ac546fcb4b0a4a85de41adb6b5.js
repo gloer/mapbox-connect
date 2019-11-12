6a5a1ddb53bd323f22cb39a5f1dea6f1edaa52f2
gloer <gloer@langslet.com>
2019-11-12 14:24:31 +0100

    Updates

---
+++ precache-manifest.fc13f1ac546fcb4b0a4a85de41adb6b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ec05d0c2441a8e0e3bc65465dda048",
+    "url": "/mapbox-connect/index.html"
+  },
+  {
+    "revision": "6fca6b878ef8a4fb585c",
+    "url": "/mapbox-connect/static/css/2.d9a8a5a7.chunk.css"
+  },
+  {
+    "revision": "29f5df1f6e1593a2122e",
+    "url": "/mapbox-connect/static/css/main.39c3be40.chunk.css"
+  },
+  {
+    "revision": "6fca6b878ef8a4fb585c",
+    "url": "/mapbox-connect/static/js/2.e622b811.chunk.js"
+  },
+  {
+    "revision": "29f5df1f6e1593a2122e",
+    "url": "/mapbox-connect/static/js/main.1b540ae2.chunk.js"
+  },
+  {
+    "revision": "9b1daab7cd0a30bc0a33",
+    "url": "/mapbox-connect/static/js/runtime-main.efd6c590.js"
+  },
+  {
+    "revision": "173391a42ba9af4a516a101c561a8f0b",
+    "url": "/mapbox-connect/static/media/marker.173391a4.svg"
+  }
+]);
